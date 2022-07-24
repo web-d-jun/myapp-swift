55fc69e9757bf9b0b47389a9570ae766216039af
web-d-jun <cjy874545@gmail.com>
2022-07-24 18:35:53 +0900

    Initial Commit

---
+++ Shared/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  Shared
+//
+//  Created by 조준영 on 2022/07/24.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
